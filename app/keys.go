15cfa43a0c59c8f91f28018a21092046893b976a
mazhuang <z.ma@bmi-tech.cn>
2022-04-18 15:14:01 +0800

    🌈 style(views): 更新提示信息

--- app/keys.go
+++ app/keys.go
@@ -2,13 +2,11 @@ package app
 
 import (
 	"lazydocker/views"
-
-	ui "github.com/gizak/termui/v3"
 )
 
 func initKeys(a *App) {
-	a.keys.Text = "Press q to quit, Press j or k to switch container"
+	a.keys.Text = "q: quit, j/↓ or k/↑, Tab: switch containers/images, h/← ok l/→ : switch navigation, s: change containers status"
 	a.keys.SetRect(0, views.TerminalHeight-1, views.TerminalWidth, views.TerminalHeight)
 	a.keys.Border = false
-	a.keys.TextStyle.Bg = ui.ColorBlue
+	a.keys.TextStyle = a.activeStyle
 }
